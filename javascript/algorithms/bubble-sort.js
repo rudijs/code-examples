f824d4e27fa70327581654a46bfeccfbc1ac429a
Rudi Starcevic <ooly.me@gmail.com>
2017-06-18 11:29:14 +0800

    Add bubble sort immutable version, add standardjs

--- javascript/algorithms/bubble-sort.js
+++ javascript/algorithms/bubble-sort.js
@@ -6,23 +6,42 @@
  * and so on and so on....
  */
 
-module.exports = bubbleSort
+exports.bubbleSort = bubbleSort
+exports.bubbleSortImmutable = bubbleSortImmutable
 
-function bubbleSort(a) {
-  var swapped;
-  var cn = 0
+function bubbleSort (a) {
+  var swapped
   do {
-    swapped = false;
-    cn++
+    swapped = false
     for (var i = 0; i < a.length - 1; i++) {
       if (a[i] > a[i + 1]) {
-        var temp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = temp;
-        swapped = true;
+        var temp = a[i]
+        a[i] = a[i + 1]
+        a[i + 1] = temp
+        swapped = true
       }
     }
-  } while (swapped);
+  } while (swapped)
 
   return a
+}
+
+function bubbleSortImmutable (a) {
+  let swapped
+  // clone input
+  const arr = a.slice(0)
+
+  do {
+    swapped = false
+    for (let i = 0; i < a.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+        swapped = true
+      }
+    }
+  } while (swapped)
+
+  return arr
 }
--- javascript/algorithms/bubble-sort.test.js
+++ javascript/algorithms/bubble-sort.test.js
@@ -1,12 +1,36 @@
 const assert = require('assert')
-const bubbleSort = require('./bubble-sort')
-
-const a = [134, 203, 3, 746, 200, 984, 198, 764, 9]
-const expected = [3, 9, 134, 198, 200, 203, 746, 764, 984]
+const bubbleSort = require('./bubble-sort').bubbleSort
+const bubbleSortImmutable = require('./bubble-sort').bubbleSortImmutable
 
 describe('bubble-sort', () => {
-  it('should sort in ascending order', () => {
-    const res = bubbleSort(a)
-    assert.deepEqual(expected, res)
+  describe('mutable', () => {
+    it('should sort in ascending order', () => {
+      const a1 = [134, 203, 3, 746, 200, 984, 198, 764, 9]
+      const expected = [3, 9, 134, 198, 200, 203, 746, 764, 984]
+
+      const res = bubbleSort(a1)
+
+      // input/source is mutated
+      assert.deepEqual(expected, a1)
+
+      // stored value is sorted
+      assert.deepEqual(expected, res)
+    })
   })
-});
+
+  describe('immutable', () => {
+    it('should sort in ascending order', () => {
+      const a1 = [134, 203, 3, 746, 200, 984, 198, 764, 9]
+      const a2 = a1.slice(0)
+      const expected = [3, 9, 134, 198, 200, 203, 746, 764, 984]
+
+      const res = bubbleSortImmutable(a1)
+
+      // input/source is not mutated
+      assert.deepEqual(a1, a2)
+
+      // stored value is sorted
+      assert.deepEqual(expected, res)
+    })
+  })
+})
